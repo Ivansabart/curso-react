5d2b7466b70bc0509faa19a2bdaebdcad7cecc67
Iván Salazar Bartolo <ivansabart@gmail.com>
2022-05-04 04:16:56 -0500

    add 02-intro-js

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+// Variables y constantes
+
+const nombre = "Iván";
+const apellido = "Salazar";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+  let nombre = "Peta";
+  console.log(nombre);
+}
