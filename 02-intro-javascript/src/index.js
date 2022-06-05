1e5dc2b34e4a94161efb8cce61de702c1dbe153c
Iván Salazar Bartolo <ivansabart@gmail.com>
2022-06-04 20:06:50 -0500

    filter

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,5 +1,10 @@
 import { heroes } from './bases/data/heroes'
+// find
+const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
 
-const getHeroeById = (id) => {
-  
-}
+console.log( getHeroeById(2) );
+
+// filter
+const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
+
+console.log( getHeroesByOwner('DC') );
