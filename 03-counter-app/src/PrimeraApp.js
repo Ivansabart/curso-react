11d266cd86e4523bfcaa681221400c8230a85bd9
ivansabart <ivansabart@gmail.com>
2022-06-07 11:59:36 -0500

    add JSON.stringify

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,10 +1,15 @@
 const PrimeraApp = () => {
+  
+  const saludo ='Hola Mundo';
+  
   return( 
     <>
-      <h1>Hola Mundo</h1>
+      <h1>{ saludo }</h1>
+      {/* <h1>{ JSON.stringify(saludo) }</h1> */}
       <p>Mi primera aplicación</p>
     </>
   );
+
 }
 
 export default PrimeraApp;
